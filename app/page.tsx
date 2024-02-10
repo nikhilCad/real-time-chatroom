d0be229d11b28f324636e0537be2e52e9c381794
nikhilcad <nikhilcaddilac@gmail.com>
2024-02-10 13:23:18 +0530

    playing around with ShadCN

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,16 @@
 // import Image from "next/image";
 
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
+    <div className="flex flex-col">
     <p className="text-3xl font-bold text-indigo-500">
     Hi
     </p>
+    <Button>
+      This is ShadcnUI button
+    </Button>
+    </div>
   );
 }
