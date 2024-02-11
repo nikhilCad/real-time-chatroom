b129cc4198fcf58d3966a8fc698d645baf5542d7
nikhilcad <nikhilcaddilac@gmail.com>
2024-02-11 13:25:08 +0530

    update auth

--- app/api/auth/[...nextauth]/options.ts
+++ app/api/auth/[...nextauth]/options.ts
@@ -1,8 +1,33 @@
-//contains the providers for authentication using next-auth
-
-import type { NextAuthOptions } from "next-auth";
+import type { NextAuthOptions } from 'next-auth'
+import GitHubProvider from 'next-auth/providers/github'
+import CredentialsProvider from 'next-auth/providers/credentials'
 
 export const options: NextAuthOptions = {
-    //providers for authentication
-    providers: [],
+    providers: [
+        GitHubProvider({
+            clientId: process.env.GITHUB_ID as string,
+            clientSecret: process.env.GITHUB_SECRET as string,
+        }),
+        CredentialsProvider({
+            name: "Credentials",
+            credentials: {
+                username: {
+                    label: "Username:",
+                    type: "text",
+                    placeholder: "your-cool-username"
+                },
+                password: {
+                    label: "Password:",
+                    type: "password",
+                    placeholder: "your-awesome-password"
+                }
+            },
+            async authorize(credentials) {
+                // (TODO) do input validation of credentials
+                //also increment the id
+
+                return {id: "1", name: credentials?.username, password: credentials?.password}
+            }
+        })
+    ],
 }
