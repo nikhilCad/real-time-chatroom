c8f59b1a146b920d3beadc911bea6f2477b38c4e
nikhilcad <nikhilcaddilac@gmail.com>
2024-02-11 14:17:49 +0530

    Add temporary signout button

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -2,6 +2,7 @@
 
 import { ModeToggle } from "@/components/mode-toggle";
 import { Button } from "@/components/ui/button";
+import Link from "next/link"
 import { cn } from "@/lib/utils";
 import { options } from "@/app/api/auth/[...nextauth]/options";
 import { getServerSession } from "next-auth";
@@ -9,6 +10,7 @@ import { getServerSession } from "next-auth";
 export default async function Home() {
 
   const session = await getServerSession(options)
+  //This page will only show when logged in
   return (
     <div>
     <p className="text-3xl font-bold text-indigo-500">
@@ -17,9 +19,14 @@ export default async function Home() {
     <Button className={cn("bg-indigo-500", true&&"bg-red-500")}>
       This is ShadcnUI button ok
     </Button>
+    <ModeToggle></ModeToggle>
+    
     <p>Hi {session?.user?.name}</p>
     <img src={session?.user?.image as string}></img>
-    <ModeToggle></ModeToggle>
+
+    <Button> <Link href="/api/auth/signout">Sign Out</Link> </Button>
+      
+    
     </div>
   );
 }
