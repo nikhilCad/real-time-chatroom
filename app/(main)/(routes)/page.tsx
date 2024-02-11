7067a5d4c26c93724ea33fb584015d84159a68b3
nikhilcad <nikhilcaddilac@gmail.com>
2024-02-11 13:44:05 +0530

    Protect site using authentication, display username on login

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -3,8 +3,12 @@
 import { ModeToggle } from "@/components/mode-toggle";
 import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
+import { options } from "@/app/api/auth/[...nextauth]/options";
+import { getServerSession } from "next-auth";
 
-export default function Home() {
+export default async function Home() {
+
+  const session = await getServerSession(options)
   return (
     <div>
     <p className="text-3xl font-bold text-indigo-500">
@@ -13,6 +17,8 @@ export default function Home() {
     <Button className={cn("bg-indigo-500", true&&"bg-red-500")}>
       This is ShadcnUI button ok
     </Button>
+    <p>Hi {session?.user?.name}</p>
+    <img src={session?.user?.image as string}></img>
     <ModeToggle></ModeToggle>
     </div>
   );
