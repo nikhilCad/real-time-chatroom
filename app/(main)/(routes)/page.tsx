e3ad3c300af8c0fdfcb2c20953afcfe2dbd69f79
nikhilcad <nikhilcaddilac@gmail.com>
2024-02-12 22:54:00 +0530

    re-add dark theme

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { UserButton } from '@clerk/nextjs'
+import { ModeToggle } from '@/components/mode-toggle'
 
 const Home = () => {
   return (
     <div>
       <UserButton afterSignOutUrl="/"/>
+      <ModeToggle/>
     </div>
     
   )
