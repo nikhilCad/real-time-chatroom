a2cabc01a05e4ac23ef6ec0ba5bdc24daad1d5a6
nikhilcad <nikhilcaddilac@gmail.com>
2024-02-17 13:24:03 +0530

    barebones modals added

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -6,17 +6,17 @@
 
 import { useEffect, useState } from "react";
 
-// import { EditServerModal } from "@/components/modals/edit-server-modal";
-// import { InviteModal } from "@/components/modals/invite-modal";
+import { EditServerModal } from "@/components/modals/edit-server-modal";
+import { InviteModal } from "@/components/modals/invite-modal";
 import { CreateServerModal } from "@/components/modals/create-server-modal";
-// import { MembersModal } from "@/components/modals/members-modal";
-// import { CreateChannelModal } from "@/components/modals/create-channel-modal";
-// import { LeaveServerModal } from "@/components/modals/leave-server-modal";
-// import { DeleteServerModal } from "@/components/modals/delete-server-modal";
-// import { DeleteChannelModal } from "@/components/modals/delete-channel-modal";
-// import { EditChannelModal } from "@/components/modals/edit-channel-modal";
-// import { MessageFileModal } from "@/components/modals/message-file-modal";
-// import { DeleteMessageModal } from "@/components/modals/delete-message-modal";
+import { MembersModal } from "@/components/modals/members-modal";
+import { CreateChannelModal } from "@/components/modals/create-channel-modal";
+import { LeaveServerModal } from "@/components/modals/leave-server-modal";
+import { DeleteServerModal } from "@/components/modals/delete-server-modal";
+import { DeleteChannelModal } from "@/components/modals/delete-channel-modal";
+import { EditChannelModal } from "@/components/modals/edit-channel-modal";
+import { MessageFileModal } from "@/components/modals/message-file-modal";
+import { DeleteMessageModal } from "@/components/modals/delete-message-modal";
 
 export const ModalProvider = () => {
   const [isMounted, setIsMounted] = useState(false);
@@ -35,7 +35,7 @@ export const ModalProvider = () => {
   return (
     <>
       <CreateServerModal />
-      {/* <InviteModal />
+      <InviteModal />
       <EditServerModal />
       <MembersModal />
       <CreateChannelModal />
@@ -44,7 +44,7 @@ export const ModalProvider = () => {
       <DeleteChannelModal />
       <EditChannelModal />
       <MessageFileModal />
-      <DeleteMessageModal /> */}
+      <DeleteMessageModal />
     </>
   )
 }
