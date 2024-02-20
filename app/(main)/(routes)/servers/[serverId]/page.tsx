d0bf882aadbd9b33463521d6335fd25fb3464292
nikhilcad <nikhilcaddilac@gmail.com>
2024-02-20 23:47:29 +0530

    add redirection to general on server enter

--- app/(main)/(routes)/servers/[serverId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/page.tsx
@@ -1,66 +1,57 @@
-//This show up at `/servers/${server.id}`
+import { redirectToSignIn } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 
-// import { redirectToSignIn } from "@clerk/nextjs";
-// import { redirect } from "next/navigation";
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
 
-// import { currentProfile } from "@/lib/current-profile";
-// import { db } from "@/lib/db";
+interface ServerIdPageProps {
+  params: {
+    serverId: string;
+  }
+};
 
-// interface ServerIdPageProps {
-//   params: {
-//     serverId: string;
-//   }
-// };
+//Default page on entering a server
 
-// const ServerIdPage = async ({
-//   params
-// }: ServerIdPageProps) => {
-//   const profile = await currentProfile();
+const ServerIdPage = async ({
+  params
+}: ServerIdPageProps) => {
+  const profile = await currentProfile();
 
-//   if (!profile) {
-//     return redirectToSignIn();
-//   }
+  if (!profile) {
+    return redirectToSignIn();
+  }
 
-//   const server = await db.server.findUnique({
-//     where: {
-//       id: params.serverId,
-//       members: {
-//         some: {
-//           profileId: profile.id,
-//         }
-//       }
-//     },
-//     include: {
-//       channels: {
-//         where: {
-//           name: "general"
-//         },
-//         orderBy: {
-//           createdAt: "asc"
-//         }
-//       }
-//     }
-//   })
+  const server = await db.server.findUnique({
+    where: {
+      id: params.serverId,
+      members: {
+        some: {
+          profileId: profile.id,
+        }
+      }
+    },
+    include: {
+      channels: {
+        where: {
+          name: "general"
+        },
+        orderBy: {
+          createdAt: "asc"
+        }
+      }
+    }
+  })
 
-//   const initialChannel = server?.channels[0];
+  //default channel to redirect is general channel
+  const initialChannel = server?.channels[0];
 
-//   if (initialChannel?.name !== "general") {
-//     return null;
-//   }
+  //we cant remove general so this check is just for sanity
+  if (initialChannel?.name !== "general") {
+    return null;
+  }
 
-//   return redirect(`/servers/${params.serverId}/channels/${initialChannel?.id}`)
-// }
- 
-// export default ServerIdPage;
-
-import React from 'react'
-
-function page() {
-    return (
-        <div>
-            Server page
-        </div>
-    )
+  //redirect to general
+  return redirect(`/servers/${params.serverId}/channels/${initialChannel?.id}`)
 }
-
-export default page
+ 
+export default ServerIdPage;
